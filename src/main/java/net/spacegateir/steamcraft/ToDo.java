546f9add791af2dd19f21d0d213301b4fbcb2282
spacegateir <1Spacegateir@gmail.com>
2025-03-25 21:56:15 +1030

    Initial commit

--- src/main/java/net/spacegateir/steamcraft/ToDo.java
+++ src/main/java/net/spacegateir/steamcraft/ToDo.java
@@ -4,6 +4,8 @@ public class ToDo {
 
     //add 6 crystals to loot tables (elder gardian, wither, dragon, warden) (?,?)
 
+    //create dimentions (earth copy, void, waterworld, skulk, overworld neather type, sky dimention)
+
     //
 
 }
